5aaf6065f4fc932b7862e6a149b2fac9c0f1fc62
Atul Varma <varmaa@gmail.com>
2012-03-15 11:10:01 -0500

    add socket.io, error alert messages

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var express = require('express'),
     config = require('./config');
 
 var app = express.createServer();
+var io = require('socket.io').listen(app);
 
 function relpath(pathname) {
   return path.join(__dirname, pathname);
@@ -11,16 +12,81 @@ function relpath(pathname) {
 
 app.use(express.static(relpath('static')));
 
-app.get('/api/token', function(request, response) {
+module.exports = app;
+
+var connections = {};
+var latestID = 0;
+
+function infoForUser(conn) {
+  return {
+    username: conn.username,
+    muted: conn.muted
+  };
+}
+
+function getInfoForAllUsers() {
+  var info = {};
+  for (var id in connections) {
+    info[id] = infoForUser(connections[id]);
+  }
+  return info;
+}
+
+io.sockets.on('connection', function(socket) {
   var capability = new TwilioCapability(config.account_sid,
                                         config.auth_token);
-  capability.allow_client_incoming("tommy");
-  capability.allow_client_outgoing(config.app_sid);
+  var conn = {
+    token: null,
+    username: "UNKNOWN",
+    id: latestID++,
+    muted: false
+  };
 
-  return response.send({
-    token: capability.generate(),
-    etherpad_url: config.etherpad_url
+  capability.allow_client_outgoing(config.app_sid);
+  conn.token = capability.generate();
+  connections[conn.id] = conn;
+
+  socket.emit('configuration', {
+    token: conn.token,
+    id: conn.id,
+    etherpad_url: config.etherpad_url,
+    users: getInfoForAllUsers()
+  });
+  socket.broadcast.emit('user-connected', {
+    id: conn.id,
+    value: infoForUser(conn)
+  });
+  socket.on('set-username', function(data) {
+    conn.username = data.value;
+    socket.broadcast.emit('set-username', {
+      id: conn.id,
+      value: conn.username
+    });
+  });
+  socket.on('set-muted', function(data) {
+    conn.muted = data.value;
+    socket.broadcast.emit('set-muted', {
+      id: conn.id,
+      value: conn.muted
+    });
+  });
+  socket.on('app-log', function(data) {
+    socket.broadcast.emit('app-log', {
+      id: conn.id,
+      message: data.message
+    });
+  });
+  socket.on('app-error', function(data) {
+    socket.broadcast.emit('app-error', {
+      id: conn.id,
+      message: data.message
+    });
+  });
+  socket.on('disconnect', function(data) {
+    delete connections[conn.id];
+    socket.broadcast.emit('user-disconnected', {
+      id: conn.id,
+    });
+    console.log("DISCONNECT");
   });
 });
-
-module.exports = app;
